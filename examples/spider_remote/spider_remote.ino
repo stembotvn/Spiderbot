b662a2eada27166d0792731afc8ed803c98d48de
Tuanhung4797 <34309959+Tuanhung4797@users.noreply.github.com>
2017-12-11 15:59:28 +0700

    example repair

--- examples/spider_remote/spider_remote.ino
+++ examples/spider_remote/spider_remote.ino
@@ -2,10 +2,30 @@
 #include "IRremote.h"
 
 #define receiverPin A0
+
+// Define IR Remote Button Codes
+#define irUp  16736925
+#define irDown 16754775
+#define irRight 16761405
+#define irLeft 16720605
+#define irOK 16712445
+#define ir1 16738455
+#define ir2 16750695
+#define ir3 16756815
+#define ir4 16724175
+#define ir5 16718055
+#define ir6 16743045
+#define ir7 16716015
+#define ir8 16726215
+#define ir9 16734885
+#define ir0 16730805
+#define irStar 16728765
+#define irPound 16732845
+
 spider robot;
 IRrecv irrecv(receiverPin);
 decode_results results;
-int dem;
+int count;
 //////////////////////////////////////
 void setup()
 {
@@ -17,64 +37,64 @@ void ReadRemote()
 {
   switch(results.value)
   {
-    case 16736925: //// tiến
-      for(dem = 0; dem < 5; dem++)
+    case irUp: //// tiến
+      for(count = 0; count < 5; count++)
       {
         robot.forward(70);
-        dem++;
+        count++;
       }
       break;
-    case 16754775: //// lùi
-      for(dem = 0; dem < 5; dem++)
+    case irDown: //// lùi
+      for(count = 0; count < 5; count++)
       {
         robot.backward(70);
-        dem++;
+        count++;
       }
       robot.backward(70);
       break;
-    case 16720605: //// trái
+    case irLeft: //// trái
       robot.turnleft(70);
       break;
-    case 16761405: //// phải
+    case irRight: //// phải
       robot.turnright(70);
       break;
-    case 16712445: //// ok
+    case irOK: //// ok
       robot.stand2();
       break;
-    case 16738455: //// 1
+    case ir1: //// 1
       robot.standUp(20);
       break;
-    case 16750695: //// 2
+    case ir2: //// 2
       robot.sleep(20);      
       break;
-    case 16756815: //// 3
+    case ir3: //// 3
       robot.stand1();      
       break;
-    case 16724175: //// 4
+    case ir4: //// 4
       robot.hello(10);
       break;
-    case 16718055: //// 5
+    case ir5: //// 5
       robot.start(20);
       break;
-    case 16743045: //// 6
+    case ir6: //// 6
       robot.exercise(20);
       break;
-    case 16716015: //// 7
+    case ir7: //// 7
       robot.layDown(20);
       break;
-    case 16726215: //// 8
+    case ir8: //// 8
       robot.stand3();
       break;
-    case 16734885: //// 9
+    case ir9: //// 9
       
       break;
-    case 16730805: //// 0
+    case ir0: //// 0
       
       break;
-    case 16728765: //// *
+    case irStar: //// *
       
       break;
-    case 16732845: //// #
+    case irPound: //// #
       
       break;
   }
