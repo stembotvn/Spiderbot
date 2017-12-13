e6c45897d2be3db8886beab83a4254fa271ed0ba
Tuanhung4797 <34309959+Tuanhung4797@users.noreply.github.com>
2017-12-13 08:57:26 +0700

    add Gesture sensor

--- examples/gesture_sensor/gesture_sensor.ino
+++ examples/gesture_sensor/gesture_sensor.ino
@@ -1,30 +1,39 @@
-#include <spider.h>
-#include "IRremote.h"
+/*
+Hardware Connections:
+IMPORTANT: The APDS-9960 can only accept 3.3V!
+ 
+ Arduino Pin  APDS-9960 Board  Function
+ 
+ 3.3V         VCC              Power
+ GND          GND              Ground
+ A4           SDA              I2C Data
+ A5           SCL              I2C Clock
+ 2            INT              Interrupt
+*/
+
 #include <Wire.h>
 #include "SparkFun_APDS9960.h"
+#include <spider.h>
 
 spider robot;
-IRrecv irrecv(receiverPin);
-decode_results results;
-
+// Pins
 #define APDS9960_INT    2 // Needs to be an interrupt pin
+
+// Constants
 
 // Global Variables
 SparkFun_APDS9960 apds = SparkFun_APDS9960();
 int isr_flag = 0;
 int count;
-//////////////////////////////////////
-void setup()
+
+void setup() 
 {
   robot.init();
-  Serial.begin(9600);
-  irrecv.enableIRIn();
-/////////////////////////////////////////
-// Set interrupt pin as input
+  // Set interrupt pin as input
   pinMode(APDS9960_INT, INPUT);
 
   // Initialize Serial port
-  //Serial.begin(9600);
+  Serial.begin(9600);
   Serial.println();
   Serial.println(F("--------------------------------"));
   Serial.println(F("SparkFun APDS-9960 - GestureTest"));
@@ -46,86 +55,9 @@ void setup()
   } else {
     Serial.println(F("Something went wrong during gesture sensor init!"));
   }
-  
 }
-void ReadRemote()
-{
-  if (irrecv.decode(&results)) // nếu nhận được tín hiệu
-  {
-    control();
-    delay(200);
-    irrecv.resume(); // nhận giá trị tiếp theo
-  }
-}
-///////////////////////////
-void control()
-{
-  switch(results.value)
-  {
-    case irUp: //// tiến
-      for(count = 0; count < 5; count++)
-      {
-        robot.forward(70);
-        count++;
-      }
-      break;
-    case irDown: //// lùi
-      for(count = 0; count < 5; count++)
-      {
-        robot.backward(70);
-        count++;
-      }
-      robot.backward(70);
-      break;
-    case irLeft: //// trái
-      robot.turnleft(70);
-      break;
-    case irRight: //// phải
-      robot.turnright(70);
-      break;
-    case irOK: //// ok
-      robot.stand1();
-      //robot.start(20);
-      break;
-    case ir1: //// 1
-      robot.standUp(20);
-      break;
-    case ir2: //// 2
-      robot.sleep(20);      
-      break;
-    case ir3: //// 3
-      robot.stand2();      
-      break;
-    case ir4: //// 4
-      robot.hello(10);
-      break;
-    case ir5: //// 5
-      robot.start(20);
-      break;
-    case ir6: //// 6
-      robot.exercise(20);
-      break;
-    case ir7: //// 7
-      robot.layDown(20);
-      break;
-    case ir8: //// 8
-      robot.stand3();
-      break;
-    case ir9: //// 9
-      
-      break;
-    case ir0: //// 0
-      
-      break;
-    case irStar: //// *
-      
-      break;
-    case irPound: //// #
-      
-      break;
-  } 
-}
-void loop()
+
+void loop() 
 {
   if( isr_flag == 1 ) {
     detachInterrupt(0);
@@ -133,13 +65,15 @@ void loop()
     isr_flag = 0;
     attachInterrupt(0, interruptRoutine, FALLING);
   }
-  ReadRemote();
 }
-void interruptRoutine() {
+
+void interruptRoutine() 
+{
   isr_flag = 1;
 }
 
-void handleGesture() {
+void handleGesture() 
+{
     if ( apds.isGestureAvailable() ) {
     switch ( apds.readGesture() ) {
       case DIR_UP:
